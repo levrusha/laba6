dee23cca45a548b501e327e204b33fd0b5f1b167
KariAlmaz <left4behind@gmail.com>
2017-03-22 16:27:31 +0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("15170003"
 		+ "15130016"
-		+ "133008"
+		+ "15130888"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
